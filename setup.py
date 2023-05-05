5a8493d4d587468070d478c41b1d861a25c02388
ingyentegla <132700179+ingyentegla@users.noreply.github.com>
2023-05-05 23:44:17 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-tradebot',
+    version='',
+    packages=['UI', 'ml', 'tests', 'scraper', 'traders', 'trading_api'],
+    url='',
+    license='',
+    author='daniel',
+    author_email='',
+    description=''
+)
